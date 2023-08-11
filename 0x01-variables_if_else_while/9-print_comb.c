9e9c0b50819085f6dc3991ec92950e2087230b0b
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-08-11 08:41:10 -0700

    print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+int n;
+n = 0;
+while (n <= 9)
+{
+	putchar ('0' + n);
+	putchar (',');
+	putchar (' ');
+	++n;
+}
+	putchar ('\n');
+return (0);
+}
