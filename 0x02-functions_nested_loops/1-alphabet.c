960d7f27b56bdc8abd2a74a141d31f16e457cf1d
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-08-15 06:01:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this function prints lowercase alpha
+ * Return: void 
+ */
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+	_putchar(alpha);
+}
+	_putchar('\n');
+	}
