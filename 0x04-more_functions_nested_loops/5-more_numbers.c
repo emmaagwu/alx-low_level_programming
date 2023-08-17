b718fe241e33615f220f1c258f516adb70e5bdb4
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-08-17 02:33:08 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints 10 times
+ *
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	n = 1;
+	while (n <= 10)
+	{
+		for(i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+			_putchar(i + '0');
+			}
+		}
+		_putchar('\n');
+		n++;
+	}
+}
