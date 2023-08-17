46260c8ca3fdedc34baaeae5ffce0e5e4282468a
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-08-17 06:15:17 -0700

    7.printing_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,23 @@
 
 void print_diagonal(int n)
 {
-	int i;
-	int j;
-
-	if (n != 0)
+	if (n <= 0)
 	{
-		j = 1;
-		i = 1
-		while (j <= n)
-		{
-			_putchar(' ');
-			if (i == j)
-			{
-		_putchar(92);
 		_putchar('\n');
-			}
-		j++;
-		i++;
+	} else
+	{	
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+		for (j = 0; j < n; j++)
+		{
+			if (j == i)
+			_putchar(92);
+			else if  (j < i)
+			_putchar(' ');
 		}
-	 _putchar('\n');
+		_putchar('\n');
+		}
 	}
 }
