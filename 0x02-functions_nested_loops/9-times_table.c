ba70a290534c3985c9353858b38bbc23b597b557
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-08-18 21:24:27 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,16 @@ void times_table(void)
 {
 int step;
 int n;
-int m;
 
 for (step = 0; step <= 9; step++)
 {
 	for (n = 0; n <= 9; n++)
 	{
+
+	int m;
+
 	m = n * step;
+
 	if (m > 9)
 	{
 	_putchar((m / 10) + '0');
@@ -25,19 +28,16 @@ for (step = 0; step <= 9; step++)
 	{
 	_putchar(m + '0');
 	}
-	if (n != 9)
-	{
-	if (m > 9)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	else
+	if (n != 9 && (step * (n + 1) <= 9))
 	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
 	}
+	else if (n != 9 && (step * (n + 1) > 9))
+	{
+	_putchar(',');
+	_putchar(' ');
 	}
 	else
 	{
@@ -45,5 +45,4 @@ for (step = 0; step <= 9; step++)
 	}
 	}
 }
-return;
 }
