39e4f9190dabd3eaac949a551230f249b5e17bc3
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-08-21 08:14:16 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ void puts_half(char *str)
 		i++;
 	}
 
-	len = i;
+	len = i - 1;
 	if (len % 2 == 0)
 	{
 	for (i = (len / 2); str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);		
+		_putchar(str[i]);
 	}
 		_putchar('\n');
 	}
