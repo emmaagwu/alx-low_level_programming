cdb51097c5647e6481d0cf62db7e86371384dba2
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-09-01 05:23:19 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - the  entry point
+ *@argc: the argument count
+ *@argv: the argument array
+ *Return: Always 0
+ *
+ */
+
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
+
