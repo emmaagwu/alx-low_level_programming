6f34a2694ca82476b15847f296f802fc84946b8d
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-09-01 05:06:31 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - the  entry point
+ *@argc: the argument count
+ *@argv: the argument array
+ *Return: Always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
