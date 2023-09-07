46ab49c5d860ccb20842e40b7860c5ff9e9d7a59
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-09-07 05:03:38 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (len1 = 0; s1[len1];)
 		len1++;
