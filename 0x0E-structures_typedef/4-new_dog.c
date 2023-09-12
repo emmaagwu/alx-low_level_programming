98b92eca5dc65d52c88eae25b826f07bb3a2164f
Agwu Emmanuel <emmanuelagwu89@gmail>
2023-09-12 06:46:25 -0700

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,9 +7,11 @@
  *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 	while (s[i] != '\0')
 {
@@ -31,17 +33,18 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 	while (src[len] != '\0')
 	{
 		len++;
 	}
-		for (i = 0; i < len; i++)
-		{
-			dest[i] = src[i];
-		}
-			dest[i] = '\0';
-			return (dest);
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 /**
