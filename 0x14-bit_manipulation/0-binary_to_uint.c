8391f1b7afc6133fea0de14468684c7e15d538fa
emmaagwu <emmanuelagwu89@gmail.com>
2023-09-28 14:24:03 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,9 +14,10 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	
+
 	for (i = 0; b[i]; i++)
-	{	if (b[i] < '0' || b[i] > '1')
+	{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		dec = 2 * dec + (b[i] - '0');
 	}
