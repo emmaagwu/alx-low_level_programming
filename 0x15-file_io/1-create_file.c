a3b98dc55cc98d5285565c65bdb252495cbde2ee
emmaagwu <emmanuelagwu89@gmail.com>
2023-10-02 09:08:29 -0700

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  *create_file - creates a file
  *@filename: name of the file to create
  *@text_content: NULL terminated string to write to the file
- *Returns: 1 on success, -1 on failure
+ *Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		fileId = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
 	else
-		fileId = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);	
+		fileId = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
 	if (fileId == -1)
 	{
 		if (errno == EEXIST)
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fileId == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 		charWritten = write(fileId, text_content, strlen(text_content));
 	if (charWritten == -1)
