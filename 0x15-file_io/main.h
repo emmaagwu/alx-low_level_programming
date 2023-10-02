b3e26169ec267a68b5251473c93d878b6747a5b7
emmaagwu <emmanuelagwu89@gmail.com>
2023-10-02 09:05:34 -0700

    1-create_file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
