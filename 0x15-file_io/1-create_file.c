e1e6b3c3ac0e377909bf29c381726d014b62a5f6
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2023-10-30 14:24:17 -0700

    1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *create_file - creates a file
+ *@filename: name of the file to create
+ *@text_content: NULL terminated string to write to the file
+ *Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fileId, charWritten;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		fileId = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	else
+		fileId = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
+	if (fileId == -1)
+	{
+		if (errno == EEXIST)
+			fileId = open(filename, O_WRONLY | O_TRUNC);
+
+	}
+
+	if (fileId == -1)
+	return (-1);
+
+	if (text_content != NULL)
+		charWritten = write(fileId, text_content, strlen(text_content));
+	if (charWritten == -1)
+	{
+		close(fileId);
+		return (-1);
+	}
+
+close(fileId);
+return (1);
+}
